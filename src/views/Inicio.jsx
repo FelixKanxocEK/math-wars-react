6fcc9b4bea933778373c0553798ba05e682d0131
Felix Kanxoc <felix kanxoc>
2023-03-24 11:45:01 -0500

    change: characters and resize elements

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -113,11 +113,11 @@ const Inicio = () => {
   app.stage.addChild(buttonMute);
 
   //Image: person image 
-  const texture = PIXI.Texture.from('../src/assets/Shot_1.png');
+  const texture = PIXI.Texture.from('../src/assets/characters/shot_1.png');
   const imageDemo = new PIXI.Sprite(texture);
   
-  imageDemo.width = app.screen.width / 6
-  imageDemo.height = app.screen.height / 4
+  imageDemo.width = app.screen.width / 5
+  imageDemo.height = app.screen.height / 3  
   imageDemo.x = app.screen.height / 5;
   imageDemo.y = app.screen.width / 4;
   imageDemo.interactive = true
@@ -129,21 +129,25 @@ const Inicio = () => {
   app.stage.addChild(imageDemo)
 
   //Text layer 
+  const widthLayer = app.screen.width / 20;
+  const heightLayer = app.screen.height / 1.2;
   const layer = new PIXI.Graphics();
   layer.lineStyle(2, 0x232323, 1)
   layer.beginFill(0xffffff);
-  layer.drawRect(50, 620, 600, 100);
+  layer.drawRect(widthLayer, heightLayer, 600, 100);
   layer.endFill();
   app.stage.addChild(layer)
   
   //Text layer: Answer 1
+  const widthLayer1 = app.screen.width / 2;
+  const heightLayer1 = app.screen.height / 1.145;
   const layer2 = new PIXI.Graphics();
   const subLayer = new PIXI.Graphics();
   const subLayer2 = new PIXI.Graphics();
 
   layer2.lineStyle(2, 0x303030, 1);
   layer2.beginFill(0xffffff);
-  layer2.drawRect(600, 520, 200, 50);
+  layer2.drawRect(widthLayer1, heightLayer1, 200, 50);
   layer2.endFill();
   layer2.cursor = 'pointer';
 
@@ -181,10 +185,12 @@ const Inicio = () => {
   
 
   //text layer: Answer 2
+  const widthLayer2 = app.screen.width / 1.5;
+  const heightLayer2 = app.screen.height / 1.144;
   const layer3 = new PIXI.Graphics();
   layer3.lineStyle(2, 0x232323, 1);
   layer3.beginFill(0xffffff);
-  layer3.drawRect(600, 590, 200, 50);
+  layer3.drawRect(widthLayer2, heightLayer2, 200, 50);
   layer3.endFill();
   layer3.cursor = 'pointer';
   //Button state: second answer
@@ -210,10 +216,12 @@ const Inicio = () => {
 
   app.stage.addChild(layer3)
   //text layer: answer 3
+  const widthLayer3 = app.screen.width / 1.2;
+  const heightLayer3 = app.screen.height / 1.144;
   const layer4 = new PIXI.Graphics();
   layer4.lineStyle(2, 0x232323, 1);
   layer4.beginFill(0xffffff);
-  layer4.drawRect(600, 660, 200, 50);
+  layer4.drawRect(widthLayer3, heightLayer3, 200, 50);
   layer4.endFill();
   layer4.cursor = 'pointer';
   //Button state: third answer
@@ -240,23 +248,23 @@ const Inicio = () => {
 
   //text
   const textQuestion = new PIXI.Text('Question 1: ')
-  textQuestion.x = 70;
-  textQuestion.y = 630;
+  textQuestion.x = app.screen.width / 15;
+  textQuestion.y = app.screen.height / 1.13;
   app.stage.addChild(textQuestion)
   //text: Answer 1
   const textAnswer1 = new PIXI.Text('Answer 1')
-  textAnswer1.x = 610;
-  textAnswer1.y = 530;
+  textAnswer1.x = app.screen.width / 1.87;
+  textAnswer1.y = app.screen.height / 1.13;
   app.stage.addChild(textAnswer1)
   //Text: Answer 2
   const textAnswer2 = new PIXI.Text('Answer 2')
-  textAnswer2.x = 610;
-  textAnswer2.y = 600;
+  textAnswer2.x = app.screen.width / 1.44;
+  textAnswer2.y = app.screen.height / 1.13;
   app.stage.addChild(textAnswer2)
   //Text: Answer 3
   const textAnswer3 = new PIXI.Text('Answer 3')
-  textAnswer3.x = 610;
-  textAnswer3.y = 670;
+  textAnswer3.x = app.screen.width / 1.16;
+  textAnswer3.y = app.screen.height / 1.13;
   app.stage.addChild(textAnswer3)
   //Text: Correct
   const textCorrect = new PIXI.Text('Correcto')
@@ -327,8 +335,8 @@ const Inicio = () => {
   const heartIcon = new PIXI.Sprite(container1);
   heartIcon.x = app.screen.height / 6.5;
   heartIcon.y = app.screen.width / 16;
-  heartIcon.height = app.screen.height / 30;
-  heartIcon.width = app.screen.width / 50;
+  heartIcon.height = 26;
+  heartIcon.width = 26;
   app.stage.addChild(heartIcon)
 
   //General panel by the first character
@@ -337,7 +345,7 @@ const Inicio = () => {
 
   //Bar Panel: General settings 
   //panel1: size
-  const widhtPanel = app.screen.width / 1.31;
+  const widhtPanel = app.screen.width / 1.32;
   const heightPanel = app.screen.height / 12;
   //first panel
   const panelBar = new PIXI.Graphics();
@@ -347,7 +355,7 @@ const Inicio = () => {
   panelBar.endFill();
   panel.addChild(panelBar)
   //panel2: size
-  const widhtPanel2 = app.screen.width / 1.322;
+  const widhtPanel2 = app.screen.width / 1.33;
   const heightPanel2 = app.screen.height / 14.5;
   //second panel
   const secondPanel = new PIXI.Graphics();
@@ -370,8 +378,8 @@ const Inicio = () => {
 
   //name for the second Character
   const secondName = new PIXI.Text('Name: Maikol', styleName);
-  secondName.x = app.screen.width / 1.28;
-  secondName.y = app.screen.height / 11;
+  secondName.x = app.screen.width / 1.27;
+  secondName.y = app.screen.height / 11.5;
   app.stage.addChild(secondName)
 
   //health bar: second character
@@ -379,70 +387,82 @@ const Inicio = () => {
   app.stage.addChild(healthBar2);
 
   //line for health bar
-  const widthHealth  = app.screen.width / 1.271;
+  const widthHealth  = app.screen.width / 1.27;
   const heightHealth = app.screen.height / 7.5;
+  const widthBar = app.screen.width / 9;
+  const heightBar = app.screen.height / 36 
   const linestyle2 = new PIXI.Graphics();
   linestyle2.lineStyle(2, 0x232323, 1);
   linestyle2.beginFill(0x808080)
-  linestyle2.drawRect(widthHealth, heightHealth, 150, 20);
+  linestyle2.drawRect(widthHealth, heightHealth, widthBar, heightBar);
   healthBar2.addChild(linestyle2)
   //red bar 2
-  const widthRed = app.screen.width / 1.270;
+  const widthRed = app.screen.width / 1.269;
   const heightRed = app.screen.height / 7.4;
+  const widthBarRed = app.screen.width / 13;
+  const heightBarRed = app.screen.height / 41;
   const redBar2 = new PIXI.Graphics();
   redBar2.beginFill(0xFF3300);
-  redBar2.drawRect(widthRed, heightRed, 100, 18)
+  redBar2.drawRect(widthRed, heightRed, widthBarRed, heightBarRed)
   redBar2.endFill();
   healthBar2.addChild(redBar2)
 
   //heart Icon 2
   const container2 = PIXI.Texture.from('../src/assets/heart.png');
   const heartIcon2 = new PIXI.Sprite(container2);
-  heartIcon2.x = app.screen.height / 0.677;
-  heartIcon2.y = app.screen.width / 16;
-  heartIcon2.height = app.screen.height / 30;
-  heartIcon2.width = app.screen.width /  50;
+  heartIcon2.x = app.screen.width / 1.31
+  heartIcon2.y = 90
+  heartIcon2.width = 26
+  heartIcon2.height = 26
   app.stage.addChild(heartIcon2)
 
-  //life icon
-  // const heartIcon = new PIXI.Graphics();
-  // heartIcon.geometry(10, 10 ,100)
-  // heartIcon.beginFill(0x232323);
-  // heartIcon(heartIcon)
+  //second chracter
+
+  const secondCharacter = PIXI.Texture.from('../src/assets/characters/character1.png')
+  const imageSecond = new PIXI.Sprite(secondCharacter)
+  imageSecond.x = app.screen.width / 1.3
+  imageSecond.y = app.screen.height / 2.1
+  imageSecond.width = 300
+  imageSecond.height = 300
+  app.stage.addChild(imageSecond)
+
+  
+
+  
 
   //Image jet: Json file 
-  PIXI.Assets.load('../src/assets/fighter.json').then(() => {
-    const frames = [];
+  // PIXI.Assets.load('../src/assets/characters/fighter.json').then(() => {
+  //   const frames = [];
 
-    for (let i = 0; i < 30; i++) {
-      const val = i < 10 ? `0${i}` : i;
+  //   for (let i = 0; i < 30; i++) {
+  //     const val = i < 10 ? `0${i}` : i;
 
-      frames.push(PIXI.Texture.from(`rollSequence00${val}.png`));
-    }
+  //     frames.push(PIXI.Texture.from(`rollSequence00${val}.png`));
+  //   }
 
-    const anim = new PIXI.AnimatedSprite(frames);
+  //   const anim = new PIXI.AnimatedSprite(frames);
 
-    anim.width = app.screen.width / 6
-    anim.height = app.screen.height / 4
-    anim.x = app.screen.width / 1.23;
-    anim.y = app.screen.height / 1.6;
-    anim.anchor.set(0.5);
-    anim.animationSpeed = 0.5;
-    // anim.play();
+  //   anim.width = app.screen.width / 6
+  //   anim.height = app.screen.height / 4
+  //   anim.x = app.screen.width / 1.23;
+  //   anim.y = app.screen.height / 1.6;
+  //   anim.anchor.set(0.5);
+  //   anim.animationSpeed = 0.5;
+  //   anim.play();
 
-    app.stage.addChild(anim);
+  //   app.stage.addChild(anim);
 
-    app.ticker.add(() => {
-      // anim.rotation += 0.01;
-    })
+  //   app.ticker.add(() => {
+  //     anim.rotation += 0.01;
+  //   })
 
-    //Button state by the second character 
-    anim.interactive = true
+  //   Button state by the second character 
+  //   anim.interactive = true
 
-    anim.on('click', function(){
-      console.log('hola')
-    })
-  })
+  //   anim.on('click', function(){
+  //     console.log('hola')
+  //   })
+  // })
   // return (
   //   <div>
 
